cad8370ccfe04ac8f720af9f77b12df1ff0fccef
Asha <ashalbenjamin@gmail.com>
2020-03-17 20:40:32 -0500

    everything works-mvp

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 //express import
 const express = require('express');
-const postsRoutes = require('./posts/postRoutes.js');
+
+const postsRoutes = require('./posts/postsRoutes.js');
 
 const server = express();
-const port = 8000;
 
 server.use(express.json());
+
 server.use('/api/posts', postsRoutes);
 
-server.listen(8000, () => {
-	console.log('API running on port 8000');
-});
+server.listen(8000, () => console.log('API running on port 8000'));
